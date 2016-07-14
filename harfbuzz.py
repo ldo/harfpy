81a652173d0fc37e6a9f70e4658ab9352781363b
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2016-07-14 01:49:05 +0000

    add Language and part of Buffer

--- harfbuzz.py
+++ harfbuzz.py
@@ -4,7 +4,7 @@
 
 import ctypes as ct
 
-harfbuzz = ct.cdll.LoadLibrary("libharfbuzz.so.0")
+hb = ct.cdll.LoadLibrary("libharfbuzz.so.0")
 
 class HARFBUZZ :
     "useful definitions adapted from harfbuzz/*.h. You will need to use the constants" \
@@ -21,21 +21,89 @@ class HARFBUZZ :
 
     bool_t = ct.c_int
     codepoint_t = ct.c_uint
+    position_t = ct.c_int
+    mask_t = ct.c_uint
+
+    class var_int_t(ct.Union) :
+        _fields_ = \
+            [
+                ("u32", ct.c_uint),
+                ("i32", ct.c_int),
+                ("u16", ct.c_ushort),
+                ("i16", ct.c_short),
+                ("u8", ct.c_ubyte),
+                ("i8", ct.c_byte),
+            ]
+    #end var_int_t
+
+    tag_t = ct.c_uint
+
+    direction_t = ct.c_uint
+    DIRECTION_INVALID = 0
+    DIRECTION_LTR = 4
+    DIRECTION_RTL = 5
+    DIRECTION_TTB = 6
+    DIRECTION_BTT = 7
+
+    script_t = ct.c_uint
+    # SCRIPT_xxx values todo
+
+    # from hb-buffer.h:
+    buffer_content_type_t = ct.c_uint
+    BUFFER_CONTENT_TYPE_INVALID = 0
+    BUFFER_CONTENT_TYPE_UNICODE = 1
+    BUFFER_CONTENT_TYPE_GLYPHS = 2
 
     # more TBD
 
 #end HARFBUZZ
+HB = HARFBUZZ # if you prefer
 
 #+
 # Routine arg/result types
 #-
 
-harfbuzz.hb_version.restype = None
-harfbuzz.hb_version.argtypes = (ct.POINTER(ct.c_uint), ct.POINTER(ct.c_uint), ct.POINTER(ct.c_uint))
-harfbuzz.hb_version_atleast.restype = HARFBUZZ.bool_t
-harfbuzz.hb_version_atleast.argtypes = (ct.c_uint, ct.c_uint, ct.c_uint)
-harfbuzz.hb_version_string.restype = ct.c_char_p
-harfbuzz.hb_version_string.argtypes = ()
+hb.hb_version.restype = None
+hb.hb_version.argtypes = (ct.POINTER(ct.c_uint), ct.POINTER(ct.c_uint), ct.POINTER(ct.c_uint))
+hb.hb_version_atleast.restype = HB.bool_t
+hb.hb_version_atleast.argtypes = (ct.c_uint, ct.c_uint, ct.c_uint)
+hb.hb_version_string.restype = ct.c_char_p
+hb.hb_version_string.argtypes = ()
+hb.hb_language_from_string.restype = ct.c_void_p
+hb.hb_language_from_string.argtypes = (ct.c_char_p, ct.c_int)
+hb.hb_language_to_string.restype = ct.c_char_p
+hb.hb_language_to_string.argtypes = (ct.c_void_p,)
+hb.hb_language_get_default.restype = ct.c_void_p
+hb.hb_language_get_default.argtypes = ()
+hb.hb_buffer_destroy.restype = None
+hb.hb_buffer_create.restype = ct.c_void_p
+hb.hb_buffer_create.argtypes = ()
+hb.hb_buffer_destroy.restype = None
+hb.hb_buffer_destroy.argtypes = (ct.c_void_p,)
+hb.hb_buffer_reset.restype = None
+hb.hb_buffer_reset.argtypes = (ct.c_void_p,)
+hb.hb_buffer_clear_contents.restype = None
+hb.hb_buffer_clear_contents.argtypes = (ct.c_void_p,)
+hb.hb_buffer_pre_allocate.restype = HB.bool_t
+hb.hb_buffer_pre_allocate.arg_types = (ct.c_void_p, ct.c_uint)
+hb.hb_buffer_allocation_successful.restype = HB.bool_t
+hb.hb_buffer_allocation_successful.argtypes = (ct.c_void_p,)
+hb.hb_buffer_add.restype = None
+hb.hb_buffer_add.argtypes = (ct.c_void_p, HB.codepoint_t, ct.c_uint)
+hb.hb_buffer_add_codepoints.restype = None
+hb.hb_buffer_add_codepoints.argtypes = (ct.c_void_p, ct.c_void_p, ct.c_int, ct.c_uint, ct.c_int)
+hb.hb_buffer_set_content_type.restype = None
+hb.hb_buffer_set_content_type.argtypes = (ct.c_void_p, HB.buffer_content_type_t)
+hb.hb_buffer_get_content_type.restype = HB.buffer_content_type_t
+hb.hb_buffer_get_content_type.argtypes = (ct.c_void_p,)
+hb.hb_buffer_set_direction.restype = None
+hb.hb_buffer_set_direction.argtypes = (ct.c_void_p, HB.direction_t)
+hb.hb_buffer_get_direction.restype = HB.direction_t
+hb.hb_buffer_get_direction.argtypes = (ct.c_void_p,)
+hb.hb_buffer_set_script.restype = None
+hb.hb_buffer_set_script.argtypes = (ct.c_void_p, HB.script_t)
+hb.hb_buffer_get_script.restype = HB.script_t
+hb.hb_buffer_get_script.argtypes = (ct.c_void_p,)
 # more TBD
 
 #+
@@ -49,7 +117,7 @@ def version() :
     major = ct.c_uint()
     minor = ct.c_uint()
     micro = ct.c_uint()
-    harfbuzz.hb_version(ct.byref(major), ct.byref(minor), ct.byref(micro))
+    hb.hb_version(ct.byref(major), ct.byref(minor), ct.byref(micro))
     return \
         major.value, minor.value, micro.value
 #end version
@@ -57,13 +125,175 @@ def version() :
 def version_atleast(major, minor, micro) :
     "is the HarfBuzz version at least that specified."
     return \
-        bool(harfbuzz.hb_version_atleast(major, minor, micro))
+        bool(hb.hb_version_atleast(major, minor, micro))
 #end version_atleast
 
 def version_string() :
     "returns the version string."
     return \
-        harfbuzz.hb_version_string().decode()
+        hb.hb_version_string().decode()
 #end version_string
+
+# from hb-common.h:
+
+class Language :
+    "wraps the hb_language_t opaque type. Do not instantiate directly; use" \
+    " the from_string() and default() methods."
+
+    __slots__ = \
+        ( # to forestall typos
+            "_hbobj",
+        )
+
+    def __init__(self, hbobj) :
+        self._hbobj = hbobj
+    #end __init__
+
+    # no __del__: are these objects ever freed?
+
+    @classmethod
+    def default(celf) :
+        return \
+            Language(hb.hb_language_get_default())
+    #end default
+
+    @classmethod
+    def from_string(celf, s) :
+        sb = s.encode()
+        return \
+            Language(hb.hb_language_from_string(sb, len(sb)))
+    #end from_string
+
+    def to_string(self) :
+        return \
+            hb.hb_language_to_string(self._hbobj).decode() # automatically stops at NUL?
+    #end to_string
+
+    def __repr__(self) :
+        return \
+            "<Language>(%s)" % self.to_string()
+    #end __repr__
+
+#end Language
+
+# from hb-buffer.h:
+
+class Buffer :
+    "a HarfBuzz buffer. Do not instantiate directly; call the create method."
+
+    __slots__ = \
+        ( # to forestall typos
+            "_hbobj",
+        )
+
+    def __init__(self, hbobj) :
+        self._hbobj = hbobj
+    #end __init__
+
+    def __del__(self) :
+        if self._hbobj != None :
+            hb.hb_buffer_destroy(self._hbobj)
+            self._hbobj = None
+        #end if
+    #end __del__
+
+    @staticmethod
+    def create() :
+        "creates a new Buffer."
+        return \
+            Buffer(hb.hb_buffer_create())
+    #end create
+
+    def reset(self) :
+        "resets the buffer state as though it were newly created."
+        hb.hb_buffer_reset(self._hbobj)
+    #end reset
+
+    def clear_contents(self) :
+        "similar to reset, but does not clear the Unicode functions" \
+        " or the replacement code point."
+        hb.hb_clear_contents(self._hbobj)
+    #end clear_contents
+
+    def pre_allocate(self, size) :
+        if not hb.hb_buffer_pre_allocate(self._hbobj, size) :
+            raise RuntimeError("pre_allocate failed")
+        #end if
+    #end pre_allocate
+
+    def allocation_successful(self) :
+        return \
+            hb.hb_buffer_allocation_successful(self._hbobj)
+    #end allocation_successful
+
+    def check_alloc(self) :
+        "checks that the last buffer allocation was successful."
+        if not hb.hb_buffer_allocation_successful(self._hbobj) :
+            raise RuntimeError("allocation failure")
+        #end if
+    #end check_alloc
+
+    def add(self, codepoint, cluster) :
+        "adds a single codepoint to the buffer, with the specified cluster index."
+        hb.hb_buffer_add(self._hbobj, codepoint, cluster)
+        self.check_alloc()
+    #end add
+
+    def add_codepoints(self, text, text_length, item_offset, item_length) :
+        "adds a list or tuple of codepoints to the buffer."
+        c_text = (text_length * HB.codepoint_t)()
+        for i in range(text_length) :
+            c_text[i] = text[i]
+        #end for
+        hb.hb_buffer_add_codepoints(self._hbobj, c_text, text_length, item_offset, item_length)
+        self.check_alloc()
+    #end add_codepoints
+
+    def add_str(self, text, text_length, item_offset, item_length) :
+        self.add_codepoints \
+          (
+            text = tuple(ord(c) for c in text),
+            text_length = text_length,
+            item_offset = item_offset,
+            item_length = item_length
+          )
+    #end add_str
+
+    @property
+    def content_type(self) :
+        return \
+            hb.hb_buffer_get_content_type(self._hbobj)
+    #end content_type
+
+    @content_type.setter
+    def content_type(self, content_type) :
+        hb.hb_buffer_set_content_type(self._hbobj, content_type)
+    #end content_type
+
+    @property
+    def direction(self) :
+        return \
+            hb.hb_buffer_get_direction(self._hbobj)
+    #end direction
+
+    @direction.setter
+    def direction(self, direction) :
+        hb.hb_buffer_set_direction(self._hbobj, direction)
+    #end direction
+
+    @property
+    def script(self) :
+        return \
+            hb.hb_buffer_get_script(self._hbobj)
+    #end script
+
+    @script.setter
+    def script(self, script) :
+        hb.hb_buffer_set_script(self._hbobj, script)
+    #end script
+
+    # more TBD
+
+#end Buffer
 
 # more TBD
