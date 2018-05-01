bfd4e65c8f2caa1dc6d390b28c492efbaa676ae4
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2018-05-01 03:49:33 +0000

    add config for uploading to PyPI

--- setup.py
+++ setup.py
@@ -34,10 +34,13 @@ distutils.core.setup \
   (
     name = "HarfPy",
     version = "0.8",
-    description = "language bindings for HarfBuzz, for Python 3.3 or later",
+    description = "language bindings for HarfBuzz",
+    long_description =
+        "language bindings for the HarfBuzz shaper library, for Python 3.3 or later",
     author = "Lawrence D'Oliveiro",
     author_email = "ldo@geek-central.gen.nz",
     url = "http://github.com/ldo/harfpy",
+    license = "LGPL v2.1+",
     py_modules = ["harfbuzz"],
     cmdclass =
         {
